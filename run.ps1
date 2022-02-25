86104f9fa84bd10902d6bb5dfe651b55d0cbfe6d
Pu Xu <puxu@microsoft.com>
2022-02-25 14:20:21 +0800

    fix

--- run.ps1
+++ run.ps1
@@ -1,12 +1,18 @@
 param(
-    [switch]$Detach
+    [switch]$Detach,
+
+    [Parameter(ValueFromRemainingArguments)]
+    [string[]]$Remaining
 )
 
 $env:UPTIME_KUMA_PORT = 5003
+$exe = node
+$Arguments = @(".\server\server.js") + $Arguments
 
 Push-Location $PSScriptRoot
 try {
-    Start-Process node .\server\server.js -Wait:$(-not $Detach) -NoNewWindow:$(-not $Detach)
+    Start-Process $exe -ArgumentList @Arguments `
+        -Wait:$(-not $Detach) -NoNewWindow:$(-not $Detach)
 }
 finally {
     Pop-Location
